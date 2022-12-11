529dadb13407cef3a9604a3a48b105d8268d6de2
Nikita Davydov <nikita.davidov02@yandex.ru>
2022-12-11 19:56:07 +0300

    Initial commit

---
+++ Расчёт энергии решётки слоистого перовскита/Program.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Расчёт_энергии_решётки_слоистого_перовскита
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double a = 3.871 * Math.Pow(10, -10);
+            //double b = 3.871 * Math.Pow(10, -10);
+            //double c = 29.782 * Math.Pow(10, -10);
+
+            //double a = 3.898 * Math.Pow(10, -10);
+            //double b = 3.898 * Math.Pow(10, -10);
+            //double c = 30.502 * Math.Pow(10, -10);
+
+            double a = 3.871 * Math.Pow(10, -10);
+            double b = 3.871 * Math.Pow(10, -10);
+            double c = 30.502 * Math.Pow(10, -10);
+
+            //double a = 5.63874 * Math.Pow(10, -10);
+            //double b = 5.63874 * Math.Pow(10, -10);
+            //double c = 5.63874 * Math.Pow(10, -10);
+
+            //double a = 1;
+            //double b = 1;
+            //double c = 1;
+            double e = 1.6 * Math.Pow(10, -19);
+            //double e = 1;
+            double k = 9 * Math.Pow(10, 9);
+            double Na = 6.02 * Math.Pow(10, 23);
+            int z = 2;
+
+            double U = 0;
+            double M = 0;
+
+            Console.WriteLine("Введите диапозон (радиус расчёта в элементарных ячейках)");
+            int diapozon = Convert.ToInt32(Console.ReadLine());
+
+
+            Dictionary<Vector3, double> cellAtoms = new Dictionary<Vector3, double>();
+
+
+            ////1.Задание координат атомов
+
+            //cellAtoms.Add(new Vector3(a / 2, a / 2, a / 2), e / 8);
+            //cellAtoms.Add(new Vector3(a / 2, -a / 2, a / 2), e / 8);
+            //cellAtoms.Add(new Vector3(-a / 2, a / 2, a / 2), e / 8);
+            //cellAtoms.Add(new Vector3(-a / 2, -a / 2, a / 2), e / 8);
+
+            //cellAtoms.Add(new Vector3(a / 2, a / 2, -a / 2), e / 8);
+            //cellAtoms.Add(new Vector3(a / 2, -a / 2, -a / 2), e / 8);
+            //cellAtoms.Add(new Vector3(-a / 2, a / 2, -a / 2), e / 8);
+            //cellAtoms.Add(new Vector3(-a / 2, -a / 2, -a / 2), e / 8);
+
+            //cellAtoms.Add(new Vector3(0, 0, a / 2), e / 2);
+            //cellAtoms.Add(new Vector3(0, 0, -a / 2), e / 2);
+            //cellAtoms.Add(new Vector3(0, a / 2, 0), e / 2);
+            //cellAtoms.Add(new Vector3(0, -a / 2, 0), e / 2);
+            //cellAtoms.Add(new Vector3(a / 2, 0, 0), e / 2);
+            //cellAtoms.Add(new Vector3(-a / 2, 0, 0), e / 2);
+
+            //cellAtoms.Add(new Vector3(0, 0, 0), -e);
+
+            //cellAtoms.Add(new Vector3(a / 2, a / 2, 0), -e / 4);
+            //cellAtoms.Add(new Vector3(a / 2, -a / 2, 0), -e / 4);
+            //cellAtoms.Add(new Vector3(-a / 2, a / 2, 0), -e / 4);
+            //cellAtoms.Add(new Vector3(-a / 2, -a / 2, 0), -e / 4);
+
+            //cellAtoms.Add(new Vector3(0, a / 2, a / 2), -e / 4);
+            //cellAtoms.Add(new Vector3(0, -a / 2, a / 2), -e / 4);
+            //cellAtoms.Add(new Vector3(a / 2, 0, a / 2), -e / 4);
+            //cellAtoms.Add(new Vector3(-a / 2, 0, a / 2), -e / 4);
+
+            //cellAtoms.Add(new Vector3(0, a / 2, -a / 2), -e / 4);
+            //cellAtoms.Add(new Vector3(0, -a / 2, -a / 2), -e / 4);
+            //cellAtoms.Add(new Vector3(a / 2, 0, -a / 2), -e / 4);
+            //cellAtoms.Add(new Vector3(-a / 2, 0, -a / 2), -e / 4);
+
+            ////1.1.1 Задание координат атомов калия в ячейке
+            cellAtoms.Add(new Vector3(0, 0, (c - 3 * a) / 2), e);
+            cellAtoms.Add(new Vector3(0, 0, -(c - 3 * a) / 2), e);
+
+            cellAtoms.Add(new Vector3(a / 2, a / 2, 3 * a / 2), e/4);
+            cellAtoms.Add(new Vector3(a / 2, -a / 2, 3 * a / 2), e/4);
+            cellAtoms.Add(new Vector3(-a / 2, a / 2, 3 * a / 2), e/4);
+            cellAtoms.Add(new Vector3(-a / 2, -a / 2, 3 * a / 2), e/4);
+            cellAtoms.Add(new Vector3(a / 2, a / 2, -3 * a / 2), e/4);
+            cellAtoms.Add(new Vector3(a / 2, -a / 2, -3 * a / 2), e/4);
+            cellAtoms.Add(new Vector3(-a / 2, a / 2, -3 * a / 2), e/4);
+            cellAtoms.Add(new Vector3(-a / 2, -a / 2, -3 * a / 2), e/4);
+
+            ////1.1.2 Задание координат атомов лантана в ячейке
+            cellAtoms.Add(new Vector3(0, 0, (c - a) / 2), 3 * e);
+            cellAtoms.Add(new Vector3(0, 0, -(c - a) / 2), 3 * e);
+
+            cellAtoms.Add(new Vector3(a / 2, a / 2, a / 2), 3 * e / 4);
+            cellAtoms.Add(new Vector3(a / 2, -a / 2, a / 2), 3 * e /4);
+            cellAtoms.Add(new Vector3(-a / 2, a / 2, a / 2), 3 * e /4);
+            cellAtoms.Add(new Vector3(-a / 2, -a / 2, a / 2), 3 * e /4);
+            cellAtoms.Add(new Vector3(a / 2, a / 2, -a / 2), 3 * e /4);
+            cellAtoms.Add(new Vector3(a / 2, -a / 2, -a / 2), 3 * e /4);
+            cellAtoms.Add(new Vector3(-a / 2, a / 2, -a / 2), 3 * e /4);
+            cellAtoms.Add(new Vector3(-a / 2, -a / 2, -a / 2), 3 * e /4);
+
+            ////1.1.3 Задание координат атомов титана в ячейке
+            cellAtoms.Add(new Vector3(0, 0, 0), 4 * e);
+
+            cellAtoms.Add(new Vector3(0, 0, a), 4 * e);
+            cellAtoms.Add(new Vector3(0, 0, -a), 4 * e);
+
+            cellAtoms.Add(new Vector3(a / 2, a / 2, (c/2)-a), e);
+            cellAtoms.Add(new Vector3(a / 2, -a / 2, (c / 2) - a), e);
+            cellAtoms.Add(new Vector3(-a / 2, a / 2, (c / 2) - a), e);
+            cellAtoms.Add(new Vector3(-a / 2, -a / 2, (c / 2) - a), e);
+            cellAtoms.Add(new Vector3(a / 2, a / 2, a-(c/2)), e);
+            cellAtoms.Add(new Vector3(a / 2, -a / 2, a - (c / 2)), e);
+            cellAtoms.Add(new Vector3(-a / 2, a / 2, a - (c / 2)), e);
+            cellAtoms.Add(new Vector3(-a / 2, -a / 2, a - (c / 2)), e);
+
+            cellAtoms.Add(new Vector3(a / 2, a / 2, c / 2), e / 2);
+            cellAtoms.Add(new Vector3(a / 2, -a / 2, c / 2), e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, a / 2, c / 2), e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, -a / 2, c / 2), e / 2);
+            cellAtoms.Add(new Vector3(a / 2, a / 2, -c / 2), e / 2);
+            cellAtoms.Add(new Vector3(a / 2, -a / 2, -c / 2), e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, a / 2, -c / 2), e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, -a / 2, -c / 2), e / 2);
+
+            ////1.1.4 Задание координат атомов кислорода в ячейке
+            cellAtoms.Add(new Vector3(0, 0, a / 2), -2 * e);
+            cellAtoms.Add(new Vector3(0, 0, 3 * a / 2), -2 * e);
+            cellAtoms.Add(new Vector3(0, 0, -a / 2), -2 * e);
+            cellAtoms.Add(new Vector3(0, 0, -3 * a / 2), -2 * e);
+
+            cellAtoms.Add(new Vector3(a / 2, 0, c / 2), -e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, 0, c / 2), -e / 2);
+            cellAtoms.Add(new Vector3(0, a/2, c / 2), -e / 2);
+            cellAtoms.Add(new Vector3(0 , -a/2, c / 2), -e / 2);
+            cellAtoms.Add(new Vector3(a / 2, 0, -c / 2), -e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, 0, -c / 2), -e / 2);
+            cellAtoms.Add(new Vector3(0, a / 2, -c / 2), -e / 2);
+            cellAtoms.Add(new Vector3(0, -a / 2, -c / 2), -e / 2);
+
+            cellAtoms.Add(new Vector3(a / 2, a / 2, (c-a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(a / 2, -a / 2, (c - a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, a / 2, (c - a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, -a / 2, (c - a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(a / 2, a / 2, -(c - a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(a / 2, -a / 2, -(c - a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, a / 2, -(c - a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, -a / 2, -(c - a) / 2), -e / 2);
+
+            cellAtoms.Add(new Vector3(a / 2, 0, (c / 2)-a), -e);
+            cellAtoms.Add(new Vector3(-a / 2, 0, (c / 2) - a), -e);
+            cellAtoms.Add(new Vector3(0, a / 2, (c / 2) - a), -e);
+            cellAtoms.Add(new Vector3(0, -a / 2, (c / 2) - a), -e);
+            cellAtoms.Add(new Vector3(a / 2, 0, a-(c/2)), -e);
+            cellAtoms.Add(new Vector3(-a / 2, 0, a - (c / 2)), -e);
+            cellAtoms.Add(new Vector3(0, a / 2, a - (c / 2)), -e);
+            cellAtoms.Add(new Vector3(0, -a / 2, a - (c / 2)), -e);
+
+            cellAtoms.Add(new Vector3(a / 2, a / 2, (c - 3 * a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(a / 2, -a / 2, (c - 3 * a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, a / 2, (c - 3 * a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, -a / 2, (c - 3 * a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(a / 2, a / 2, -(c - 3 * a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(a / 2, -a / 2, -(c - 3 * a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, a / 2, -(c - 3 * a) / 2), -e / 2);
+            cellAtoms.Add(new Vector3(-a / 2, -a / 2, -(c - 3 * a) / 2), -e / 2);
+
+            cellAtoms.Add(new Vector3(a / 2, 0, a), -e);
+            cellAtoms.Add(new Vector3(-a / 2, 0, a), -e);
+            cellAtoms.Add(new Vector3(0, a / 2, a), -e);
+            cellAtoms.Add(new Vector3(0, -a / 2, a), -e);
+            cellAtoms.Add(new Vector3(a / 2, 0, -a), -e);
+            cellAtoms.Add(new Vector3(-a / 2, 0, -a), -e);
+            cellAtoms.Add(new Vector3(0, a / 2, -a), -e);
+            cellAtoms.Add(new Vector3(0, -a / 2, -a), -e);
+
+            cellAtoms.Add(new Vector3(a / 2, 0, 0), -e);
+            cellAtoms.Add(new Vector3(-a / 2, 0, 0), -e);
+            cellAtoms.Add(new Vector3(0, a / 2, 0), -e);
+            cellAtoms.Add(new Vector3(0, -a / 2, 0), -e);
+
+            //2. Энергия решётки
+            Vector3 center = new Vector3(a/2, 0, 0);
+
+            foreach (Vector3 cellAtom in cellAtoms.Keys)
+            {
+                if(cellAtom.x==0 && cellAtom.y == 0 && cellAtom.z == 0)
+                {
+
+                }
+                M = 0;
+                double DU = 0;
+                for (int i = -diapozon; i <= diapozon; i++)
+                {
+                    for (int j = -diapozon; j <= diapozon; j++)
+                    {
+                        for (int ko = -diapozon; ko <= diapozon; ko++)
+                        {
+                            double deltaX = i * a;
+                            double deltaY = j * b;
+                            double deltaZ = ko * c;
+                            Vector3 deltaPosition = new Vector3(deltaX, deltaY, deltaZ);
+                            foreach (Vector3 anotherAtom in cellAtoms.Keys)
+                            {
+                                Vector3 anotherAtomPosition = VectorMath.Sum(anotherAtom, deltaPosition);
+                                double distance = VectorMath.Magnitude(VectorMath.Difference(anotherAtomPosition, cellAtom));
+                                if (distance != 0)
+                                {
+                                    double dU = cellAtoms[cellAtom] * cellAtoms[anotherAtom] / distance;
+                                    //Console.WriteLine(distance/a);
+                                    double dM = (a * cellAtoms[anotherAtom]) / (2 * distance * e);
+                                    M += dM;
+                                    if (Math.Abs(dM) > 1)
+                                    {
+                                        //Console.WriteLine("Help!" + dM);
+                                    }
+                                    U += dU/2;
+                                    DU += dU/2;
+                                }
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine(cellAtom.x/a + " " + cellAtom.y / a + " " + cellAtom.z / a);
+                Console.WriteLine(Na * k * DU);
+                Console.WriteLine(Na * k * U);
+                //Console.WriteLine("M = " + M);
+            }
+
+            //for (int i = -diapozon; i<= diapozon; i ++)
+            //{
+            //    for (int j = -diapozon; j <= diapozon; j++)
+            //    {
+            //        for (int ko = -diapozon; ko <= diapozon; ko++)
+            //        {
+            //            //Сдвиг
+            //            double deltaX = i * a;
+            //            double deltaY = j * b;
+            //            double deltaZ = ko * c;
+            //            Vector3 deltaPosition = new Vector3(deltaX, deltaY, deltaZ);
+            //            //foreach(Vector3 cellAtom in cellAtoms.Keys)
+            //            //{
+            //            //    foreach(Vector3 anotherAtom in cellAtoms.Keys)
+            //            //    {
+            //            //        Vector3 anotherAtomPosition = VectorMath.Sum(anotherAtom, deltaPosition);
+            //            //        double distance = VectorMath.Magnitude(VectorMath.Difference(anotherAtomPosition, cellAtom));
+            //            //        if (distance != 0)
+            //            //        {
+            //            //            double dU = (k * cellAtoms[cellAtom] * cellAtoms[anotherAtom]) / distance;
+            //            //            U += dU / 2;
+            //            //        }
+            //            //    }
+            //            //}
+
+
+
+
+            //            //foreach (Vector3 anotherAtom in cellAtoms.Keys)
+            //            //{
+            //            //Vector3 anotherAtomPosition = VectorMath.Sum(anotherAtom, deltaPosition);
+            //            //double distance = VectorMath.Magnitude(VectorMath.Difference(anotherAtomPosition, center));
+            //            //    if (distance != 0)
+            //            //    {
+            //            //        double dU = cellAtoms[anotherAtom] / distance;
+            //            //        double dM = (a * cellAtoms[anotherAtom]) / (2 * distance * e);
+            //            //        M += dM;
+            //            //        if (Math.Abs(dM) > 1)
+            //            //        {
+            //            //            Console.WriteLine("Help!");
+            //            //        }
+            //            //        U += dU;
+            //            //    }
+            //            //}
+            //        }
+            //    }
+            //}
+            double Um = Na * k * U;
+            Console.WriteLine("U = " + (Um / 1000) + " кДж/моль");
+            Console.WriteLine("M = " + M);
+        }
+    }
+}
---
+++ Расчёт энергии решётки слоистого перовскита/Vector.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Расчёт_энергии_решётки_слоистого_перовскита
+{
+    public struct Vector3
+    {
+        public double x;
+        public double y;
+        public double z;
+        public Vector3(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+    public static class VectorMath
+    {
+        public static Vector3 Sum(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+        public static Vector3 Scalar(double k, Vector3 a)
+        {
+            return new Vector3(a.x * k, a.y * k, a.z * k);
+        }
+        public static double Magnitude(Vector3 v)
+        {
+            return (double)Math.Sqrt((v.x * v.x) + (v.y * v.y) + (v.z * v.z));
+        }
+        public static Vector3 Difference(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+        public static Vector3 ClampMagnitude(Vector3 v, double magnitude)
+        {
+            if (v.x != 0)
+            {
+                double a = v.y / v.x;
+                double b = v.z / v.x;
+                double x = magnitude / ((double)Math.Sqrt(1 + a * a + b * b));
+                double y = a * x;
+                double z = b * x;
+                if (v.x * x < 0)
+                    x = -x;
+                if (v.y * y < 0)
+                    y = -y;
+                if (v.z * z < 0)
+                    z = -z;
+                return new Vector3(x, y, z);
+            }
+            else if (v.y != 0)
+            {
+                double a = v.x / v.y;
+                double b = v.z / v.y;
+                double y = magnitude / ((double)Math.Sqrt(1 + a * a + b * b));
+                double x = a * y;
+                double z = b * y;
+                if (v.x * x < 0)
+                    x = -x;
+                if (v.y * y < 0)
+                    y = -y;
+                if (v.z * z < 0)
+                    z = -z;
+                return new Vector3(x, y, z);
+            }
+            else if (v.z != 0)
+            {
+                double a = v.y / v.z;
+                double b = v.x / v.z;
+                double z = magnitude / ((double)Math.Sqrt(1 + a * a + b * b));
+                double y = a * z;
+                double x = b * z;
+                if (v.x * x < 0)
+                    x = -x;
+                if (v.y * y < 0)
+                    y = -y;
+                if (v.z * z < 0)
+                    z = -z;
+                return new Vector3(x, y, z);
+            }
+            else
+                return new Vector3(0, 0, 0);
+        }
+        public static bool Equals(Vector3 a, Vector3 b)
+        {
+            if (a.x == b.x && a.y == b.y && a.z == b.z)
+                return true;
+            else
+                return false;
+        }
+    }
+}
